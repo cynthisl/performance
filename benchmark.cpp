4f57478845bce7eb870abd77f3b1cb403dd1c9bc
cynthia <cynthisl@users.noreply.github.com>
2018-10-21 17:24:06 -0700

    added all base data strucs

--- benchmark.cpp
+++ benchmark.cpp
@@ -7,6 +7,9 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <math.h>
+#include <list>
+#include <set>
 
 using namespace std;
 using namespace std::chrono;
@@ -19,6 +22,24 @@ auto timeFunction = [](auto && F, auto && ... params) {
     return duration_cast<microseconds>(stop - start).count();
 };
 
+auto calcMean(vector<long int> vals) {
+    auto sum = std::accumulate(vals.begin(), vals.end(), 0);
+    return sum/vals.size();
+}
+auto calcStddev(vector<long int> vals) {
+    long int mean = calcMean(vals);
+    std::vector<long int> diffs(vals.size());
+    std::transform(vals.begin(), vals.end(), diffs.begin(),
+            [mean](long int x){return x - mean;});
+    auto sum = std::inner_product(diffs.begin(), diffs.end(), diffs.begin(), 0);
+    return std::sqrt(sum/vals.size());
+}
+
+auto calcMedian(vector<long int> vals) {
+    std::nth_element(vals.begin(), vals.begin() + vals.size()/2, vals.end());
+    return vals[vals.size()/2];
+}
+
 class BenchmarkInterface {
     public:
     /*clock_t timing(void(BenchmarkInterface::*func)()) {
@@ -26,9 +47,13 @@ class BenchmarkInterface {
         (*func)();
         return clock() - start;
     }*/
+    long size;
+    long searchVal;
     virtual void iterate() = 0;
+    virtual void insertSetup() = 0;
     virtual void insert() = 0;
     virtual void search() = 0;
+    virtual void eraseSetup() = 0;
     virtual void erase() = 0;
 
     // I know all of these are the same, but struggling with passing in class function pointers.
@@ -39,6 +64,7 @@ class BenchmarkInterface {
         return duration_cast<microseconds>(stop - start).count();
     }
     auto timeInsert() {
+        insertSetup();
         auto start = high_resolution_clock::now();
         insert();
         auto stop = high_resolution_clock::now();
@@ -51,6 +77,7 @@ class BenchmarkInterface {
         return duration_cast<microseconds>(stop - start).count();
     }
     auto timeErase() {
+        eraseSetup();
         auto start = high_resolution_clock::now();
         erase();
         auto stop = high_resolution_clock::now();
@@ -65,39 +92,57 @@ class BenchmarkInterface {
     }
 
     void repeat(string command, int times) {
-        long int results[times];
+        vector<long int> results(times);
+        int warmup = 25;
+
         if(command == "iterate") {
+            for(int i=0; i<warmup; i++) {
+                timeIterate();
+            }
             for(int i=0; i<times; i++) {
                 results[i] = timeIterate();
             }
         }
-        if(command == "insert") {
+        else if(command == "insert") {
+            for(int i=0; i<warmup; i++) {
+                timeInsert();
+            }
             for(int i=0; i<times; i++) {
                 results[i] = timeInsert();
             }
         }
-        if(command == "search") {
+        else if(command == "search") {
+            for(int i=0; i<warmup; i++) {
+                timeSearch();
+            }
             for(int i=0; i<times; i++) {
                 results[i] = timeSearch();
             }
         }
-        if(command == "erase") {
+        else if(command == "erase") {
+            for(int i=0; i<warmup; i++) {
+                timeErase();
+            }
             for(int i=0; i<times; i++) {
                 results[i] = timeErase();
             }
         }
 
         cout << "Running " << command << " " << times << "times:" << endl;
-        for(int i=0; i<times; i++) {
+        /*for(int i=0; i<times; i++) {
             cout << results[i] << endl;
-        }
+        }*/
+        cout << "Mean: " << calcMean(results) << endl;
+        cout << "Median: " << calcMedian(results) << endl;
+        cout << "Stddev: " << calcStddev(results) << endl;
+
     }
 };
 
 class VectorBenchmark : public BenchmarkInterface {
     public:
     vector<long> testVector;
-    long size;
+    vector<long> tmpVector;
 
     VectorBenchmark(long elements) {
         size = elements;
@@ -107,6 +152,7 @@ class VectorBenchmark : public BenchmarkInterface {
                 [max = elements]() {
                 return (rand() % max);
                 });
+        searchVal = rand() % elements;
     }
 
     void iterate() {
@@ -118,22 +164,27 @@ class VectorBenchmark : public BenchmarkInterface {
         }
     }
 
+    void insertSetup() {
+        tmpVector = testVector;
+    }
     void insert() {
-        testVector.push_back(-1);
+        tmpVector.push_back(-1);
     }
 
     void search() {
-        long item = rand() % size;
-        find(testVector.begin(), testVector.end(), item);
+        find(testVector.begin(), testVector.end(), searchVal);
     }
 
+    void eraseSetup() {
+        tmpVector = testVector;
+    }
     void erase() {
         long item = rand() % size;
-        for(auto it=testVector.begin();
-               it != testVector.end();
+        for(auto it=tmpVector.begin();
+               it != tmpVector.end();
               ++it) {
            if(*it == item) {
-              testVector.erase(it);
+              tmpVector.erase(it);
               break;
            }
         }
@@ -141,44 +192,146 @@ class VectorBenchmark : public BenchmarkInterface {
 
 };
 
-//class ListBenchmark : public BenchmarkIterface {
-//};
+class ListBenchmark : public BenchmarkInterface {
+    public:
+    std::list<long int> testList;
+    std::list<long int> tmpList;
+
+    ListBenchmark(long elements) {
+        size = elements;
+        testList.resize(elements);
+        std::generate(testList.begin(),
+                testList.end(), 
+                [max = elements]() {
+                return (rand() % max);
+                });
+        searchVal = rand() % elements;
+    }
+
+    void iterate() {
+        long count = 0;
+        for(auto it=testList.begin();
+                it!= testList.end();
+                ++it) {
+            count = *it;
+        }
+    }
+
+    void insertSetup() {
+        tmpList = testList;
+    }
+    void insert() {
+        tmpList.push_back(-1);
+    }
+
+    void search() {
+        find(testList.begin(), testList.end(), searchVal);
+    }
+
+    void eraseSetup() {
+        tmpList = testList;
+    }
+    void erase() {
+        long item = rand() % size;
+        for(auto it=tmpList.begin();
+               it != tmpList.end();
+              ++it) {
+           if(*it == item) {
+              tmpList.erase(it);
+              break;
+           }
+        }
+    } 
+
+
+};
 
 class MapBenchmark : public BenchmarkInterface {
     public:
-    std::map<int, int> testMap;
+    std::map<long, int> testMap;
+    std::map<long, int> tmpMap;
+    long searchVal;
 
     MapBenchmark(long elements) {
-        testMap[0] = 1;
-        testMap[1] = 2;
+        size = elements;
+        for(long i=0; i<size; i++) {
+            testMap[i] = 1;
+        }
+        searchVal = rand() % size;
     }
         
-
-
     void iterate() {
         int count = 0;
-        for(std::map<int, int>::iterator it=testMap.begin();
+        for(std::map<long, int>::iterator it=testMap.begin();
                 it!=testMap.end();
                 ++it) {
             count = it->first;
         }
     }
 
+    void insertSetup() {
+        tmpMap = testMap;
+    }
     void insert() {
-        testMap.insert(std::pair<int, int>(100, 100));
+        tmpMap.insert(std::pair<long, int>(100, 100));
+    }
+
+    void search() {
+        testMap.find(searchVal);
+    }
+    void eraseSetup() {
+        tmpMap = testMap;
+    }
+    void erase() {
+        tmpMap.erase(searchVal);
     }
 
 };
 
 
-//class SetBenchmark : public BenchmarkIterface {
+class SetBenchmark : public BenchmarkInterface {
+    public:
+    std::set<long> testSet;
+    std::set<long> tmpSet;
+    long searchVal;
 
-//};
-//
+    SetBenchmark(long elements) {
+        size = elements;
+        for(long i=0; i<size; i++) {
+            testSet.insert(i);
+        }
+        searchVal = rand() % size;
+    }
+        
+    void iterate() {
+        int count = 0;
+        for(auto it=testSet.begin();
+                it!=testSet.end();
+                ++it) {
+            count = *it;
+        }
+    }
+
+    void insertSetup() {
+        tmpSet = testSet;
+    }
+    void insert() {
+        tmpSet.insert(100);
+    }
+
+    void search() {
+        testSet.find(searchVal);
+    }
+    void eraseSetup() {
+        tmpSet = testSet;
+    }
+    void erase() {
+        tmpSet.erase(searchVal);
+    }
+};
 
 
-
-int main() {
+int main(int argc, char* argv[]) {
 
     /*
      * inputs:
@@ -186,9 +339,9 @@ int main() {
      *  percent iterate, insert, search, delete
      */
     
-    VectorBenchmark v(100000);
+    VectorBenchmark v(pow(10, 6));
     v.run();
-    v.repeat("iterate", 100);
+    v.repeat("search", 1000);
 
     return 0;
 }
